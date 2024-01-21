c1a217bb4a885f62a726108532e488380c2a66c8
Constelium.xyz <constelium.xyz@gmail.com>
2024-01-21 17:10:57 +0100

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom"; // Importez useLocation
 import "./Header.css";
 import logo from "../../assets/LogoConst.png";
 
 function Header() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const location = useLocation(); // Utilisez le hook pour accéder à l'objet de localisation
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
 
+  const shouldShowDigitalCanvasLink = location.pathname !== "/OurCanvases"; // Condition pour l'affichage
+
   return (
-      <header>
-        <nav>
-          <button id="menuButton" className="menu-button" onClick={togglePopup}>
-            <i className="icon">☰</i>
-          </button>
-          {isPopupOpen && (
-            <div id="dropdownMenu" className="dropdown-menu">
-              <a href="/">Home</a>
-              <a href="/OurCanvases">Canvas</a>
-              <a href="/AboutUs">About us</a>
-              <a href="/Faq">FAQ</a>
-              <a href="/Contact">Contact us</a>
-            </div>
-          )}
-        </nav>
-        <img className="logo" src={logo} width="80" height="80" />
-      </header>
+    <header>
+      <nav>
+        {shouldShowDigitalCanvasLink && ( // Utilisez la condition ici pour afficher ou masquer le lien
+          <div className="header-middle">
+            <a href="/OurCanvases" className="header-link">
+              Get your digital canvas
+            </a>
+          </div>
+        )}
+        {/* Le reste de votre code */}
+        <button id="menuButton" className="menu-button" onClick={togglePopup}>
+          <i className="icon">☰</i>
+        </button>
+        {isPopupOpen && (
+          <div id="dropdownMenu" className="dropdown-menu">
+            <a href="/">Home</a>
+            <a href="/OurCanvases">Canvas</a>
+            <a href="/AboutUs">About us</a>
+            <a href="/Faq">FAQ</a>
+            <a href="/Contact">Contact us</a>
+          </div>
+        )}
+      </nav>
+      <img className="logo" src={logo} width="80" height="80" />
+    </header>
   );
 }
 
