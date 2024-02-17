01e7150cfde9484a69a8db822b51cb8418dc5f3f
Issou83 <issou8@gmail.com>
2024-02-17 17:58:52 +0100

    Mise à jour du header : position du 'Header middle'

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 
 function HomePage() {
   return (
-    <div>
+    <div className="containerMainHome">
       <Header />
       <main>
         {/* <div className="neon-wrapper">
@@ -17,62 +17,81 @@ function HomePage() {
           </button> */}
         <div className="title">
           <div className="content">
-            <div>
+            <div className="section">
               <h2>Welcome to the World of Constelium</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                Discover a premium visual experience with Constelium: a perfect
+                harmony between digital art and cutting-edge technology. Our
+                digital canvases, crafted with the expertise of artisan
+                cabinetmakers, are not just works of art; they are the pulsing
+                heart of your passion for cryptocurrencies and NFTs.
+              </p>
             </div>
-            <p>
-              Discover a premium visual experience with Constelium: a perfect
-              harmony between digital art and cutting-edge technology. Our
-              digital canvases, crafted with the expertise of artisan
-              cabinetmakers, are not just works of art; they are the pulsing
-              heart of your passion for cryptocurrencies and NFTs.
-            </p>
-            <div>
+            <div className="section">
               <h2>Display Your Passion</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                With Constelium, your NFT collection comes to life in the most
+                elegant setting imaginable. Each pixel on our screens is a
+                window into the worlds you've chosen to embrace. Your digital
+                works are no longer confined to ordinary displays; they are
+                illuminated, enhanced by the brilliance and clarity of our
+                high-end canvases.
+              </p>
             </div>
-            <p>
-              With Constelium, your NFT collection comes to life in the most
-              elegant setting imaginable. Each pixel on our screens is a window
-              into the worlds you've chosen to embrace. Your digital works are
-              no longer confined to ordinary displays; they are illuminated,
-              enhanced by the brilliance and clarity of our high-end canvases.
-            </p>
-            <div>
+            <div className="section">
               <h2>Monitor Your Cryptos</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                But Constelium isn't just for display; it's a dynamic dashboard
+                for your crypto investments. Follow market fluctuations in
+                real-time, and let your canvas inform you of trends without ever
+                losing sight of elegance and beauty. With Constelium,
+                performance meets art.
+              </p>
             </div>
-            <p>
-              But Constelium isn't just for display; it's a dynamic dashboard
-              for your crypto investments. Follow market fluctuations in
-              real-time, and let your canvas inform you of trends without ever
-              losing sight of elegance and beauty. With Constelium, performance
-              meets art.
-            </p>
-            <div>
+            <div className="section">
               <h2>Inclusive Technology</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                We understand the importance of accessibility. That's why our
+                canvases accept various wallets, allowing you to connect and
+                interact with your portfolio in an intuitive and secure way.
+              </p>
             </div>
-            <p>
-              We understand the importance of accessibility. That's why our
-              canvases accept various wallets, allowing you to connect and
-              interact with your portfolio in an intuitive and secure way.
-            </p>
-            <div>
+            <div className="section">
               <h2>Limitless Customization</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                Constelium gives you the choice: one base model for those who
+                appreciate simplicity, and customization options for those who
+                desire a more personal touch. Do you dream of a canvas that
+                reflects your identity ? We bring it to life for you.
+              </p>
             </div>
-            <p>
-              Constelium gives you the choice: one base model for those who
-              appreciate simplicity, and customization options for those who
-              desire a more personal touch. Do you dream of a canvas that
-              reflects your identity ? We bring it to life for you.
-            </p>
-            <div>
+            <div className="section">
               <h2>Join the Visual Revolution</h2>
+              <div className="caseImg">
+                <img src="" alt="" />
+              </div>
+              <p>
+                With Constelium, step into an era where digital art and finance
+                meet in a radiant dance, where every color and every number tell
+                your story. Welcome to your new visual home, where your NFTs and
+                cryptos reign supreme.
+              </p>
             </div>
-            <p>
-              With Constelium, step into an era where digital art and finance
-              meet in a radiant dance, where every color and every number tell
-              your story. Welcome to your new visual home, where your NFTs and
-              cryptos reign supreme.
-            </p>
           </div>
         </div>
         {/* </div> */}
