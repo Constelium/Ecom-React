8ddcaa9a06d819be8f19aa2be647cd93f2085f3a
Constelium.xyz <constelium.xyz@gmail.com>
2024-02-22 23:29:02 +0100

    update

--- src/Pages/OurCanvases/OurCanvases.jsx
+++ src/Pages/OurCanvases/OurCanvases.jsx
@@ -16,7 +16,7 @@ function OurCanvases() {
       <main className="OurCanvasesMain">
         <section className="product-page">
           <div className="product-image">
-            <img src="../public/imgtest.png" alt="Nom du Produit" />
+            <img src="" alt="Nom du Produit" />
           </div>
           <div className="product-details">
             <h1>Digital Canva</h1>
@@ -42,71 +42,90 @@ function OurCanvases() {
           )}
         </section>
         <div className="description">
-          <div>
-            <h4>Product Description</h4>
-            <p>
-              Dive into the heart of a revolutionary visual experience with our
-              canvases, where innovation meets elegance. Our digital canvas is
-              more than just a display; it's an open window to the worlds you
-              choose to embrace.
-            </p>
+          <h4>Product Description</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Avant-Garde Design</h4>
-            <p>
-              Each Constelium Canvas is meticulously crafted by our expert
-              artisans, incorporating high-precision screens within a
-              handcrafted frame that combines aesthetics with functionality.
-              With sleek contours and a slim profile, it's ready to take its
-              place in the most sophisticated of interiors.
-            </p>
+          <p>
+            Dive into the heart of a revolutionary visual experience with our
+            canvases, where innovation meets elegance. Our digital canvas is
+            more than just a display; it's an open window to the worlds you
+            choose to embrace.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Avant-Garde Design</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Cutting-Edge Technology</h4>
-            <p>
-              Equipped with the latest display technologies, it offers unmatched
-              image clarity, vibrant colors that bring each pixel to life.
-              Whether you're an aficionado of digital art or in search of a
-              dynamic platform for your cryptos and NFTs, our canvases are
-              designed to transcend your visual experience.
-            </p>
+          <p>
+            Each Constelium Canvas is meticulously crafted by our expert
+            artisans, incorporating high-precision screens within a handcrafted
+            frame that combines aesthetics with functionality. With sleek
+            contours and a slim profile, it's ready to take its place in the
+            most sophisticated of interiors.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Cutting-Edge Technology</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Limitless Personalization</h4>
-            <p>
-              We understand that every enthusiast has a unique vision. That's
-              why Constelium Canvas comes with a suite of customizable options.
-              Choose from a variety of sizes, frame finishes, and software
-              configurations to create a piece that truly resonates with your
-              personal style and technological needs.
-            </p>
+          <p>
+            Equipped with the latest display technologies, it offers unmatched
+            image clarity, vibrant colors that bring each pixel to life. Whether
+            you're an aficionado of digital art or in search of a dynamic
+            platform for your cryptos and NFTs, our canvases are designed to
+            transcend your visual experience.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Limitless Personalization</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Intuitive Connectivity</h4>
-            <p>
-              With a user-friendly interface and multi-platform connectivity, it
-              integrates seamlessly into your digital ecosystem. Display and
-              manage your NFT collections with ease, track real-time market
-              fluctuations of cryptocurrencies, or simply let yourself be
-              captivated by an art gallery that renews at your whim.
-            </p>
+          <p>
+            We understand that every enthusiast has a unique vision. That's why
+            Constelium Canvas comes with a suite of customizable options. Choose
+            from a variety of sizes, frame finishes, and software configurations
+            to create a piece that truly resonates with your personal style and
+            technological needs.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Intuitive Connectivity</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Eco-Responsible Commitment</h4>
-            <p>
-              At Constelium, we are dedicated to responsible art and technology.
-              Our canvases are designed to be energy-efficient, featuring
-              sustainable materials and a committed recycling policy.
-            </p>
+          <p>
+            With a user-friendly interface and multi-platform connectivity, it
+            integrates seamlessly into your digital ecosystem. Display and
+            manage your NFT collections with ease, track real-time market
+            fluctuations of cryptocurrencies, or simply let yourself be
+            captivated by an art gallery that renews at your whim.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Eco-Responsible Commitment</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
-          <div>
-            <h4>Join the Visual Revolution</h4>
-            <p>
-              Experience our digital canvases and reinvent the way you see the
-              world. It's much more than a product; it's a passport to the
-              future of digital art and digital asset management.
-            </p>
+          <p>
+            At Constelium, we are dedicated to responsible art and technology.
+            Our canvases are designed to be energy-efficient, featuring
+            sustainable materials and a committed recycling policy.
+          </p>
+        </div>
+        <div className="description">
+          <h4>Join the Visual Revolution</h4>
+          <div className="caseImg">
+            <img src="" alt="" width="100%" height="300px" />
           </div>
+          <p>
+            Experience our digital canvases and reinvent the way you see the
+            world. It's much more than a product; it's a passport to the future
+            of digital art and digital asset management.
+          </p>
         </div>
       </main>
       <Footer />
