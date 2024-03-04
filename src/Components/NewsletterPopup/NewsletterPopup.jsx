16c626bd00cec2aa443a64829807765e190f56ac
Constelium.xyz <constelium.xyz@gmail.com>
2024-03-04 21:48:49 +0100

    update

--- src/Components/NewsletterPopup/NewsletterPopup.jsx
+++ src/Components/NewsletterPopup/NewsletterPopup.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from "react";
 import "./NewsletterPopup.css";
+import axios from "axios";
 
 const NewsletterPopup = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [mail, setMail] = useState("");
+
+  // État pour la validation de l'email
+  const [isValidMail, setIsValidMail] = useState(false);
+
+  // Fonction pour mettre à jour l'état avec la valeur du champ de saisie
+  const handleMailChange = (event) => {
+    setMail(event.target.value);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -10,6 +20,28 @@ const NewsletterPopup = () => {
     }, 15000);
     return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    // Expression régulière simple pour la validation d'email
+    const mailRegex = /\S+@\S+\.\S+/;
+    setIsValidMail(mailRegex.test(mail));
+  }, [mail]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://api-const.vercel.app/mail/register",
+        { mail }
+      );
+      console.log("User registered:", response.data); // Utilisez response.data au lieu de response.json()
+      if (response.data.message) {
+        console.log("User registered:", response.data.message);
+      }
+    } catch (error) {
+      console.error("There was an error registering the user", error);
+    }
+  };
 
   const handleClose = () => setIsVisible(false);
 
@@ -22,8 +54,17 @@ const NewsletterPopup = () => {
           </button>
           <h2>Subscribe to Our Newsletter</h2>
           <p>Get the latest news and updates directly to your inbox.</p>
-          <form>
-            <input type="email" placeholder="Your email" />
+          <form onSubmit={handleSubmit}>
+            <input
+              id="emailInput"
+              type="email"
+              placeholder="Your email"
+              name="email"
+              value={mail}
+              onChange={handleMailChange}
+              required
+              aria-invalid={!isValidMail.toString()}
+            />
             <button type="submit">Subscribe</button>
           </form>
         </div>
