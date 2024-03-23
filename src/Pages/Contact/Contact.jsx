fb738394f5936d876e65b09e4be5b6bb71696c83
Constelium.xyz <constelium.xyz@gmail.com>
2024-03-24 00:34:16 +0100

    update

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,26 +1,48 @@
+import "./Contact.css";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-import "./Contact.css";
 import useAnalytics from "../../Components/useAnalytics/useAnalytics";
+import emailjs from "emailjs-com";
 
 function Contact() {
   useAnalytics();
 
+  const sendEmail = (e) => {
+    e.preventDefault(); // Pour éviter le rechargement de la page
+
+    emailjs
+      .sendForm(
+        "service_0autcan",
+        "template_rri80tt",
+        e.target,
+        "5bAKsxUPupy75QEtF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message envoyé avec succès!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Erreur lors de l'envoi du message. Veuillez réessayer.");
+        }
+      );
+  };
   return (
     <div>
       <Header />
       <main>
         <div className="contact">
-          <form action="traitement_contact.php" method="post">
+          <form onSubmit={sendEmail}>
             <label htmlFor="nom">Nom :</label>
-            <input type="text" id="nom" name="nom" required />
+            <input type="text" name="nom" required />
             <br />
             <label htmlFor="email">Email :</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" name="email" required />
             <br />
             <label htmlFor="message">Message :</label>
             <br />
-            <textarea id="message" name="message" rows="4" required></textarea>
+            <textarea name="message" rows="4" required></textarea>
             <br />
             <input type="submit" value="Envoyer" />
           </form>
