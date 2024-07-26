0ef1869c6196f1d2c16592d06e10e62e982bd1cd
Constelium.xyz <constelium.xyz@gmail.com>
2024-07-26 23:05:37 +0200

    update

--- src/Pages/OurCanvases/OurCanvases.jsx
+++ src/Pages/OurCanvases/OurCanvases.jsx
@@ -1,38 +1,16 @@
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-import Slider from "react-slick";
 import "./OurCanvases.css";
 import useAnalytics from "../../Components/useAnalytics/useAnalytics";
 
 function OurCanvases() {
   useAnalytics();
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <div>
       <Header />
       <main className="OurCanvasesMain">
         <section className="product-section">
-          <div className="carousel-container">
-            <Slider {...settings}>
-              <div>
-                <img src="image1.jpg" alt="Product 1" />
-              </div>
-              <div>
-                <img src="image2.jpg" alt="Product 2" />
-              </div>
-              <div>
-                <img src="image3.jpg" alt="Product 3" />
-              </div>
-            </Slider>
-          </div>
           <div className="product-details">
             <h1>Our Canvases</h1>
             <p>
