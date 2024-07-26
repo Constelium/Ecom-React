104e4cba29148808aaa773e385fe85fbb4de80fc
Constelium.xyz <constelium.xyz@gmail.com>
2024-07-26 23:40:11 +0200

    update

--- src/Pages/OurCanvases/OurCanvases.jsx
+++ src/Pages/OurCanvases/OurCanvases.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import "./OurCanvases.css";
@@ -6,11 +7,26 @@ import useAnalytics from "../../Components/useAnalytics/useAnalytics";
 function OurCanvases() {
   useAnalytics();
 
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+    alert("Product added to cart !");
+  };
+
+  // Voir screenshot pour ajouter les diéffrents prix automatiquement
+
+  const product = {
+    name: "Canvas Art",
+    price: 299.99,
+  };
+
   return (
     <div>
       <Header />
       <main className="OurCanvasesMain">
         <section className="product-section">
+          <button onClick={() => addToCart(product)}>Add to cart</button>
           <div className="product-details">
             <h1>Our Canvases</h1>
             <p>
