137c39a42f0a5955af8f095f56323ff4804be39f
Constelium.xyz <constelium.xyz@gmail.com>
2024-07-29 21:25:44 +0200

    update

--- src/Pages/OurCanvases/OurCanvases.jsx
+++ src/Pages/OurCanvases/OurCanvases.jsx
@@ -31,8 +31,7 @@ function OurCanvases() {
             <h1>Our Canvases</h1>
             <p>
               Discover the premium digital canvases by Constelium, blending art
-              and technology to offer a unique visual experience. Perfect for
-              displaying NFTs, financial charts, personal photos, and videos.
+              and technology to offer a unique visual experience.
             </p>
             <ul>
               <li>Display NFTs from different blockchains</li>
